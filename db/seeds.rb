466f036d89b2afae577c9b64ed681ec447a77704
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-01 14:54:11 -0500

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 require 'json'
+
 # require 'Dotenv'
 
 # Dotenv.load("../.env")
@@ -42,7 +43,7 @@ unsplash_img_arr = []
 unsplash_alt_arr = []
 
 while i < 10 do
-    unsplash_url= URI("https://api.unsplash.com/search/photos?page=#{i}&query=#{searchterm}&client_id=#{ENV["UNSPLASH_ID"]}")
+    unsplash_url= URI("https://api.unsplash.com/search/photos?page=#{i}&query=#{searchterm}&client_id=tiLpw6NIQBzOyXVdnNxVbVpkgLI0mCidl3M7DgbM1wc")
     unsplash_img_arr << get_unsplash_api(unsplash_url)
     i+=1
 end
@@ -50,7 +51,7 @@ end
 i = 0
 
 while i < 10 do
-    unsplash_url= URI("https://api.unsplash.com/search/photos?page=#{i}&query=#{searchterm}&client_id=#{ENV["UNSPLASH_ID"]}")
+    unsplash_url= URI("https://api.unsplash.com/search/photos?page=#{i}&query=#{searchterm}&client_id=tiLpw6NIQBzOyXVdnNxVbVpkgLI0mCidl3M7DgbM1wc")
     unsplash_alt_arr << get_alt_api(unsplash_url)
     i+=1
 end
